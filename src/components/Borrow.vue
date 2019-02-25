54645bf073af66ce7ae69d8c6a353e86ac3b54ba
bengineer19 <benjames1999@hotmail.co.uk>
2019-02-25 19:55:47 +0000

    Bind UI loan creation to chain

--- src/components/Borrow.vue
+++ src/components/Borrow.vue
@@ -17,7 +17,7 @@
                 label-for="details"
                 description="Give a description of the actual loan exchange"
               >
-                <b-form-input id="details" v-model="newLoan.details" required placeholder="Enter loan details"/>
+                <b-form-input id="details" v-model="details" required placeholder="Enter loan details"/>
               </b-form-group>
 
               <b-form-group
@@ -26,7 +26,7 @@
                 label-for="collateral"
                 description="Enter the amount of collateral put up for the loan"
               >
-                <b-form-input id="collateral" type="number" v-model="newLoan.collateral" required placeholder="Enter loan collateral (currently ETH only)"/>
+                <b-form-input id="collateral" type="number" v-model="collateral" required placeholder="Enter loan collateral (currently ETH only)"/>
               </b-form-group>
 
               <b-button type="submit" variant="primary">Submit</b-button>
@@ -34,11 +34,11 @@
           </b-tab>
 
           <!--  -->
-          <!-- TAB 2: Existing loan request -->
+          <!-- TAB 2: Existing loan requests -->
           <!--  -->
           <b-tab title="My open requests">
             <b-card-group deck>
-              <b-card v-for="(loan, counter) in loanList" v-bind:title="'Loan ' + (counter + 1)">
+              <b-card v-for="(loan, counter) in unregisteredLoanList" v-bind:title="'Loan ' + (counter + 1)">
                 <b-card-text>
                   <p>Loan detail: {{ loan.details }}</p>
                   <br>
@@ -61,90 +61,77 @@
 import Web3 from 'web3'
 import config from "../config"
 
- 
 var web3 = new Web3(new Web3.providers.HttpProvider(config.PROVIDER));
+ 
 
-var deployedContractAddr = "unknown"
-
-function deployContract(event) {
-  console.log("deploying...")
+function createLoan(newLoan) {
+  console.log("Creating new contract...")
   var contract = new web3.eth.Contract(config.CONTRACT_ABI, config.DEMO_CONTRACT_ADDRESS);
 
-  contract.deploy({
+  return contract.deploy({
     data: config.CONTRACT_BYTECODE,
-    arguments: [config.KYBER_NETWORK_PROXY]
+    arguments: [config.KYBER_NETWORK_PROXY, newLoan.collateral]
   })
-
   .send({
       from: config.DEMO_BORROWER_ADDRESS,
       gas: 1500000,
       gasPrice: '30000000000000'
-  // }, (error, transactionHash) => { ... })
   })
-  // .on('error', (error) => { ... })
-  // .on('transactionHash', (transactionHash) => { ... })
-  .on('receipt', (receipt) => {
-    console.log(receipt.contractAddress)
-    deployedContractAddr = receipt.contractAddress;
-  })
-  // .on('confirmation', (confirmationNumber, receipt) => { ... })
-  .then((newContractInstance) => {
-      console.log(newContractInstance.options.address)
-  });
+  .then();
 }
 
-function execContract(event) {
-  console.log(event)
-  // var contract = new web3.eth.Contract(config.CONTRACT_ABI, config.DEMO_CONTRACT_ADDRESS);
-  var contract = new web3.eth.Contract(config.CONTRACT_ABI, deployedContractAddr);
-  console.log(contract)
+function payCollateral(ETHAmount, contractAddr) {
+  var contract = new web3.eth.Contract(config.CONTRACT_ABI, contractAddr);
 
-  var tokenAddr = config.OMG_ADDRESS
-  var destinationAddr = config.DEMO_LENDER_ADDRESS
-
-  contract.methods.execSwap(tokenAddr, destinationAddr).send({
+  contract.methods.payCollateral().send({
     from: config.DEMO_BORROWER_ADDRESS,
-    value: Web3.utils.toWei('1', 'ether'),
-    gas: 1000000
+    value: Web3.utils.toWei(ETHAmount.toString(), 'ether'),
+    gas: config.COLLATERAL_GAS,
   })
   .then(function(res){
-      console.log(res);
+    console.log(res);
   }).catch(function(err) {
-      console.log(err);
+    // TODO: error here if insufficient funds
+    console.log(err);
   });
 }
 
-function createLoan(newLoan) {
-  console.log("Creating new loan offer")
-  console.log(newLoan.details);
-  console.log(newLoan.collateral);
-
-}
 
 export default {
   name: 'Loans',
   data: function(){
     return {
       newLoan: this.$emptyLoan,
+      collateral: 0,
+      details: "",
       tabIndex: 0
     }
   },
   computed : {
     loanList(){
       return this.$store.getters.LOANLIST
-    }
+    },
+    unregisteredLoanList(){
+      return this.$store.getters.UNREGISTERED_LOANLIST;
+    },
   },
   methods: {
     onLoanCreate(evt) {
       evt.preventDefault();
 
+      console.log("Getting contract addr: ");
+      // Create contract on chain
       createLoan(this.newLoan)
-      // Add to record of loans
-      this.$store.dispatch('SAVE_LOAN', this.newLoan)
-      // this.$loanList.push(this.newLoan);
-      console.log("Current loanList: ");
-      console.log(this.$store.getters.LOANLIST);
-      this.tabIndex++;
+      .then(inst => {
+        let newContractAddr = inst.options.address
+
+        payCollateral(this.collateral, newContractAddr);
+        
+        // Add to record of loans
+        this.newLoan.contractAddr = newContractAddr;
+        this.$store.dispatch('SAVE_LOAN', this.newLoan);
+        this.tabIndex++;
+      })
     },
   },
 }
